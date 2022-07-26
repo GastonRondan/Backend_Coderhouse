4466018eaa34c27a5533ae6b00f007e09515fca4
GastonRondan <gastonrondan912@gmail.com>
2022-07-26 12:42:42 -0300

    Entrega7

---
+++ Entrega7/index.js
@@ -0,0 +1,11 @@
+const createTableMDB = require('./createTableMDB')
+const createTableSQL3 = require('./createTableSQL')
+
+
+    try {
+        createTableMDB()
+        createTableSQL3()
+
+    } catch (err) {
+        console.log(err)
+    } 
